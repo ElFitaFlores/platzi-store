a229593c98930e88eea3296db391a7e400e9ab27
Rafael Flores <fita500@gmail.com>
2020-12-22 19:01:03 -0600

    Add snapshots

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {mount, shallow} from 'enzyme'
 import ProviderMock from '../../__mocks__/ProviderMock'
 import Header from '../../components/Header'
+import {create} from 'react-test-renderer'
 
 describe('Header', () => {
     test('Render del componente Header', () => {
@@ -20,5 +21,17 @@ describe('Header', () => {
             </ProviderMock>
         )
         expect(header.find(".Header-title").text()).toEqual('Platzi Store')
+    })
+})
+
+describe('Header snapshot', () => {
+    test('Comprobar el snapshot de Header', () => {
+        const header = create(
+            <ProviderMock>
+                <Header />
+            </ProviderMock>
+        )
+
+        expect(header.toJSON()).toMatchSnapshot()
     })
 })
